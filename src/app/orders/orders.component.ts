f2898d015029078873390a447ce08f6b657f4bf5
Wahiba BOUZYAN <ouahibabouzyan@gmail.com>
2023-10-21 20:08:03 +0100

    second commit

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -7,21 +7,26 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
-export class OrdersComponent implements OnInit{
+export class OrdersComponent implements OnInit {
   orders: any;
-  customerId!:number;
-  constructor(private http:HttpClient, private router:Router, private  route:ActivatedRoute) {
-    this.customerId=route.snapshot.params['customerId'] }
-  ngOnInit(): void {
-    this.http.get("http://localhost:9999/order-service/orders/search/byCustomerId?projection=fulOrder&customerId="+this.customerId)
-      .subscribe({
-        next:(data)=>{
-          this.orders=data;
-        },
-        error:(err)=>{}
-      })  }
+  customerId!: number;
 
-  getOrderDetails(o:any) {
-    this.router.navigateByUrl("/order-details/"+o.id);
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute) {
+    this.customerId = route.snapshot.params['customerId'];
+  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:9999/order-service/orders/search/byCustomerId?projection=fullOrder&customerId=" +this.customerId)
+      .subscribe({
+        next: (data) => {
+          this.orders = data;
+        },
+        error: (err) => {
+        }
+      });
+  }
+
+  getOrderDetails(o: any) {
+    this.router.navigateByUrl("/order-details/" + o.id);
   }
 }
